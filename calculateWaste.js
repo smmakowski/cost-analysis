de22a190336e4d5690b1952cd2742004741bd71e
smmakowski <makowski.sm@gmail.com>
2017-12-13 20:30:37 -0800

    implement createComboObj function

--- calculateWaste.js
+++ calculateWaste.js
@@ -1,46 +1,47 @@
 //NOTE: DO NOT DELETE THE BELOW COMMENT AS THIS IS THE MAIN program
 
-// // get arguments from command line
-// const args = Array.prototype.slice.call(process.argv, 2);
-// // if not enough aruguments print and exit
-// if (args.length < 3) {
-//   console.error();('ERROR: Improper number of arguments.');
-//   console.log('Please make sure you have the required arguments: Drug Name, units, \
-//   target dose without units, at least one vial size without units with price (delinated with \'/\')');
-//   console.log('Example: node calculateWaste.js myDrug mg 1000 200/4.35');
-//   process.exit();
-// }
-// // set variables for function
-// const drugName = args[0];
-//
-// const targetDose = args[1] * 1;
-//
-// // create array of objects from vials arguments
-// const vialSizes = args.slice(2).map((item) => {
-//   // if improper format notify and EXIT PROGRAM
-//   if (item.indexOf('/') === -1 || (item.indexOf('/') !== item.lastIndexOf('/'))) {
-//     console.error(`ERROR: argument '${item}' is not formatted properly.`);
-//     console.log('Please make sure that you use the following format <numerical vial size>/<vial price>');
-//     console.log('Example: 1240/923.40');
-//     process.exit();
-//   }
-//   const vial = createVialObject(item, targetDose);
-//
-//   return vial; // return Object
-// });
+// get arguments from command line
+const args = Array.prototype.slice.call(process.argv, 2);
+// if not enough aruguments print and exit
+if (args.length < 3) {
+  console.log('ERROR: Improper number of arguments.');
+  console.log('Please make sure you have the required arguments: Drug Name, units, \
+  target dose without units, at least one vial size without units with price (delinated with \'/\')');
+  console.log('Example: node calculateWaste.js myDrug mg 1000 200/4.35');
+  process.exit();
+}
+// set variables for function
+const drugName = args[0];
+
+const targetDose = args[1] * 1;
+
+// create array of objects from vials arguments
+const vialSizes = args.slice(2).map((item) => {
+  // if improper format notify and EXIT PROGRAM
+  if (item.indexOf('/') === -1 || (item.indexOf('/') !== item.lastIndexOf('/'))) {
+    console.error(`ERROR: argument '${item}' is not formatted properly.`);
+    console.log('Please make sure that you use the following format <numerical vial size>/<vial price>');
+    console.log('Example: 1240/923.40');
+    process.exit();
+  }
+  const vial = createVialObject(item, targetDose);
+
+  return vial; // return Object
+});
 
 // call calculation function
-// compareWaste(vialSizes);
+compareWaste(vialSizes);
 
-let vial1 = createVialObject("180/2175.22", 1000);
-let vial2 = createVialObject("260/3483.19", 1000);
-let vial3 = createVialObject("160/1990.89", 1000);
-let vial4 = createVialObject("210/1222.91", 1000);
-let vial5 = createVialObject("140/2002.81", 1000);
-
-let vialArr1 = [vial1, vial2, vial3, vial4, vial5];
-generateCombinations(vialArr1, 1000);
-
+// let vial1 = createVialObject("180/2175.22", 1000);
+// let vial2 = createVialObject("260/3483.19", 1000);
+// let vial3 = createVialObject("160/1990.89", 1000);
+// let vial4 = createVialObject("210/1222.91", 1000);
+// let vial5 = createVialObject("140/2002.81", 1000);
+//
+// let vialArr1 = [vial1, vial2, vial3, vial4, vial5];
+// let combosArr = generateCombinations(vialArr1, 1000);
+//
+// createVialCombinationObject(vialArr1, combosArr[0], 1000);
 /* helper function declarations */
 function createVialObject(sizePriceString, targetDose) {
   const values = sizePriceString.split("/");
@@ -58,20 +59,29 @@ function createVialObject(sizePriceString, targetDose) {
   vial.totalCost = vial.vialsUsed * vial.price;
   vial.wasteCost = (vial.waste / vial.size) * vial.price;
   // split on '/'
+  console.log('CREATED VIAL: 'vial);
   return vial;
 }
 
-// function to create combinations of vials (NEEDS WORK)
-function generateCombinations(vialCombinations, targetDose) {
-  // sort vials highest to lowest
-  let combinations = [];
 
+//TODO: make vialObject class; make vial objectCombo class
+// TODO: make them one and same class, and use arrays for both .size and .vialsUsed properties?
+  // see deadline and assess tech debt
+
+// function to create combinations of vials (NEEDS WORK)
+// trys to meet targetDoes prioritzing least amount of waste and least amount of vials used
+// TODO: get approval for this code, integrate into compare
+// TODO: create logic to generate a vial combo
+
+function generateCombinations(vialCombinations, targetDose) {
+  // declare combo array for return;
+  let combinations = [];
+  // sort vials highest to lowest
   const sortedVials = vialCombinations.sort((vial1, vial2) => {
     return vial1.size - vial2.size;
   }).reverse();
-  console.log(sortedVials)
-  // iterate through vials
-  // for each of the vials
+
+  // for each of the vials, start with that vial size and try to fill
   sortedVials.forEach((startVial, idx, arr) => {
     console.log('CURRENT STARTVIAL: ', startVial.size);
     if (idx === arr.length - 1) {
@@ -97,7 +107,7 @@ function generateCombinations(vialCombinations, targetDose) {
 
       let currCount = 0; // declar startCount for current vial
 
-      while (amountFilled < targetDose) { // keep trying to fill the vial with next size down until
+      while (amountFilled < targetDose) { // keep trying to fill the vial with next size down until you hit/exceed target
         amountFilled += currVial.size; // add the the vial and increase the count
         currCount += 1;
       }
@@ -129,13 +139,66 @@ function generateCombinations(vialCombinations, targetDose) {
         console.log('CURRENT COUNTS:', vialCounts);
       }
     }
-
   });
+  // make sure to delete all vials not used in combo
+  combinations.forEach((combo) => {
+    for (key in combo) {
+      if (combo[key] === 0) {
+        delete combo[key]
+      }
+    }
+  });
+
   console.log('~~~~~FINAL VIAL COUNTS~~~~~');
   combinations.forEach((combo) => {
     console.log(combo);
   });
-  return combinations;
+  return combinations; // returns array of combinations to be parsed
+}
+
+
+function createVialCombinationObject(sizesArr, combo, targetDose) {
+    let comboObj = {};
+    comboObj.sizes = [];
+    comboObj.vialCounts = [];
+    let totalUsed = 0;
+    let totalCount = 0;
+    let totalCost = 0;
+    // add vials used and
+    for (key in combo) {
+      let size = key * 1;
+      let amount = combo[key];
+      comboObj.sizes.push(size);
+      comboObj.vialCounts.push(amount);
+      totalCount += amount;
+      totalUsed += (size * amount); // add amount totalCost
+      totalCost += amount * findVialPriceWithSize(sizesArr, size);
+    }
+    //TODO: what is waste?
+
+    comboObj.totalUsed = totalUsed;
+    comboObj.vialsUsed = totalCount;
+    comboObj.waste = totalUsed - targetDose;
+    comboObj.totalCost = totalCost;
+    console.log('SMALLEST VIAL = ', Math.min(...comboObj.sizes));
+
+    const smallestSize = Math.min(...comboObj.sizes);
+    const pricePerVial = findVialPriceWithSize(sizesArr, smallestSize);
+    comboObj.wasteCost = (comboObj.waste / smallestSize) * pricePerVial; // propos
+
+    console.log(comboObj);
+    return comboObj;
+}
+// function to find price of vial sized, targetSize in array of vial objects
+function findVialPriceWithSize(arr, targetSize) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].size === targetSize) {
+      console.log(arr[i])
+      return arr[i].price;
+    }
+  }
+
+  return 0;
 }
 
 function findVialsWithMinWaste(vialCombinations) {
@@ -182,8 +245,12 @@ function findVialsWithMinSize(vialCombinations) {
   });
 }
 
+//TODO: add function to fin vials with least vials used();
+
 // function for figurinng out which of the vials that waste the lease volume waste the least money
-function compareWaste(vialSizes) {
+// TODO: integrate factored out functions for better readbility
+
+function compareWaste(vialSizes, targetDose) {
   let minWasteVial = vialSizes[0]; // start min at first vial
   // print information for first item in the sizes
   //console.log(`The ''${minWasteVial.size} size ${drugName.toUpperCase()}' wastes ${minWasteVial.waste} when attempting a ${targetDose} dose.`);
