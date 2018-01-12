891cd016ab8295b8dea5b4932768ab6d017da33a
smmakowski <makowski.sm@gmail.com>
2018-01-11 22:12:31 -0800

    add pricing from update email

--- 340bPriceList.js
+++ 340bPriceList.js
@@ -2,8 +2,11 @@
   Based off the 340b Price List Columnn in GPO 340b Price List 2.xsl
 */
 module.exports.data = [
+  /*
+    these are the prices from the pdf
+  */
   {name: 'orencia', sizePrices: ['250/345.34']}, // abatacept NOTE: DONE
-  {name: 'kadcycla', sizePrices: ['100/2175.22', '160/3483.89']}, // ado-trastuzumab NOTE: DONE
+  {name: 'kadcyla', sizePrices: ['100/2175.22', '160/3483.89']}, // ado-trastuzumab NOTE: DONE
   {name: 'recombinate', sizePrices: ['250/201.64', '500/403.78', '1000/797.36', '2000/1503.28']}, // antihemophilic factor NOTE DONE
   {name: 'vidaza', sizePrices: ['100/203.95']}, // azacitidine NOTE DONE
   {name: 'benlysta', sizePrices: ['120/363.11', '400/1206.76']}, // belimumab NOTE DONE
@@ -13,11 +16,11 @@ module.exports.data = [
   {name: 'kyprolis', sizePrices: ['30/729.50', '60/1314.29']}, // carfilzomib NOTE DONE
   {name: 'erbitux', sizePrices: ['100/424.57', '200/855.14']}, // cetuximab NOTE DONE
 
-  {name: 'cytoxan', sizePrices: ['500/178.46', '1000/344.63', '2000/616.06']}, // cyclophosphamide NOTE: DONE; coverted to mg for gram vials
-  {name: 'cytrabine', sizePrices: ['100/4.34', '2000/10.21']}, // only generic NOTE: DONE "20,000" is actually 2000
+  {name: 'cytoxan', sizePrices: ['500/178.46', '1000/344.63', '2000/616.06']}, // cyclophosphamide NOTE: DONE; coverted to mg for gram vials for consistency
+  {name: 'cytarabine', sizePrices: ['100/4.34', '2000/10.21']}, // only generic NOTE: DONE "20,000" is actually 2000
 
 
-  {name: 'dtic-dome', sizePrices: ['200/5.16']}, // dacarbazine NOTE DONE; says 'dtic-dome in pdf'
+  {name: 'dtic', sizePrices: ['200/5.16']}, // dacarbazine NOTE DONE; says 'dtic-dome in pdf'; reduced to dtic for finding in xsls
   {name: 'taxotere', sizePrices: ['20/18.13', '80/51.75', '160/151.99']}, // doclatexel NOTE DONE
   {name: 'adriamycin', sizePrices: ['10/2.49', '20/4.34', '50/288.69']}, // doxorubicin NOTE DONE
   {name: 'doxil', sizePrices: ['20/766.15', '50/1679.60']},// doxorubicin liposomal NOTE DONE
@@ -42,7 +45,7 @@ module.exports.data = [
   {name: 'vectibix', sizePrices: ['100/719.48', '400/2875.81']}, // panitumumab NOTE DONE
   {name: 'oncaspar', sizePrices: ['3750/0.05']}, // pegaspargase NOTE DONE
   {name: 'fotolyn', sizePrices: ['20/2447.22', '40/4894.44']}, // pralatrexate NOTE DONE
-  {name: 'cryamza', sizePrices: ['100/795.03', '500/3975.17']}, // ramucirum NOTE DONE
+  {name: 'cyramza', sizePrices: ['100/795.03', '500/3975.17']}, // ramucirumab NOTE DONE
   {name: 'rituxan', sizePrices: ['100/284.33', '500/1418.09']}, // rituximab NOTE DONE
   {name: 'actemra', sizePrices: ['80/221.64', '200/550.30', '400/1101.87']}, //tocilizumab NOTE DONE
   {name: 'hycamtin', sizePrices: ['4/31.98']}, // topotecan NOTE DONE
@@ -50,4 +53,19 @@ module.exports.data = [
   {name: 'vincasar', sizePrices: ['1/3.86', '2/7.71']}, //vincristine NOTE DONE
   {name: 'navelbine', sizePrices: ['10/7.14', '50/35.68']}, // vinorelbine NOTE DONE
   {name: 'zometa', sizePrices: ['4/65.21', '5/39.11']} // zolendric acid NOTE DONE
+
+  /*
+    these are the prices from the email
+  */
+
+  {name: 'darzalex', sizePrices: ['100/483.59', '400/1934.36']}, // daratumumab NOTE done
+  {name: 'simponi aria', sizePrices: ['50/1651.76']}, // golimumab NOTE done
+  {name: 'infed', sizePrices: ['100/26.51']}, // Iron dextran NOTE done
+  {name: 'ixempra', sizePrices: ['15/111.93', '45/3335.79']}, // ixabepilone NOTE DONE
+  {name: 'keytruda', sizePrices: ['100/4580.92']}, // pembrolizumab NOTE done
+  {name: 'yervoy', sizePrices: ['50/7067.70', '200/28270.77']}, // ipilimumab NOTE DONE
+  {name: 'idamycin', sizePrices: ['5/38.79', '10/77.59', '20/155.19']}, // idarubicin NOTE done
+  {name: 'jevtana', sizePrices: ['60/10335.22']}, //cabazitaxel NOTE done
+  {name: 'yondelis', sizePrices: ['1/2809.08']}, // trabectin NOTE done
+  {name: 'fotolyn', sizePrices: ['20/5047.97', '40/10095.94']}, //pralatrexate NOTE done
 ]
--- gpoPriceList.js
+++ gpoPriceList.js
@@ -2,8 +2,11 @@
   Based off the GPO Price List Columnn in GPO 340b Price List 2.xsl
 */
 module.exports.data = [
+  /*
+    this is the stuff from the pdf
+  */
   {name: 'orencia', sizePrices: ['250/1046.25']}, // abatacept NOTE: DONE
-  {name: 'kadcycla', sizePrices: ['100/2943.75', '160/4710.00']}, // ado-trastuzumab NOTE: DONE; Acutally 4710.00 for 160mg
+  {name: 'kadcyla', sizePrices: ['100/2943.75', '160/4710.00']}, // ado-trastuzumab NOTE: DONE; Acutally 4710.00 for 160mg
   {name: 'recombinate', sizePrices: ['265/283.60', '309/330.68', '505/540.44', '957/1024.15', '1040/1112.98', '1120/1198.59', '1140/1220.00', '1540/1648.06', '1570/1680.17', '1650/1765.78', '2210/2365.08', '2220/2375.78']}, // antihemophilic factor NOTE DONE
   {name: 'vidaza', sizePrices: ['100/154.55']}, // azacitidine NOTE DONE
   {name: 'benlysta', sizePrices: ['120/504.25', '400/1680.77']}, // belimumab NOTE DONE
@@ -13,14 +16,14 @@ module.exports.data = [
   {name: 'kyprolis', sizePrices: ['30/1074.38', '60/2148.75']}, // carfilzomib NOTE DONE
   {name: 'erbitux', sizePrices: ['100/606.52', '200/1213.04']}, // cetuximab NOTE DONE
   {name: 'cytoxan', sizePrices: ['500/209.26', '1000/418.52', '2000/837.94']}, // cyclophosphamide NOTE: DONE; coverted to mg for gram vials
-  {name: 'cytrabine', sizePrices: ['100/23.35', '2000/14.92']}, // only generic NOTE: DONE "20,000" is actually 2000
-  {name: 'dtic-dome', sizePrices: ['200/8.82']}, // dacarbazine NOTE DONE
+  {name: 'cytarabine', sizePrices: ['100/23.35', '2000/14.92']}, // only generic NOTE: DONE "20,000" is actually 2000
+  {name: 'dtic', sizePrices: ['200/8.82']}, // dacarbazine NOTE DONE
   {name: 'taxotere', sizePrices: ['20/24.25', '80/97.00', '160/202.83']}, // doclatexel NOTE DONE
   {name: 'adriamycin', sizePrices: ['10/6.45', '20/12.91', '50/14.95']}, // doxorubicin NOTE DONE
   {name: 'doxil', sizePrices: ['20/646.05', '50/1612.63']},// doxorubicin liposomal NOTE DONE
   {name: 'soliris', sizePrices: ['300/6523.00']}, // eculizumab NOTE DONE
   {name: 'halaven', sizePrices: ['1/1088.00']}, // eribulin NOTE DONE
-  {name: 'ferrlecit', sizePrices: ['62.5/31.80']}, // ferric sodium gluconate complex  NOTE DONE
+  {name: 'ferric sodium gluconate', sizePrices: ['62.5/31.80']}, // ferric sodium gluconate complex  NOTE DONE; changes name to generic since brand not detected
   {name: 'feraheme', sizePrices: ['510/734.40']}, // ferumoxytol NOTE DONE
   {name: 'fluororacil', sizePrices: ['500/1.99', '2500/7.79']}, // no brand NOTE DONE
   {name: 'gemzar', sizePrices: ['200/41.54', '1000/45.92', '2000/39.20']}, // gemcitabine NOTE DONE
@@ -37,7 +40,7 @@ module.exports.data = [
   {name: 'vectibix', sizePrices: ['100/1153.97', '400/4731.29']}, // panitumumab NOTE DONE
   {name: 'oncaspar', sizePrices: ['3750/14544.54']}, // pegaspargase NOTE DONE
   {name: 'fotolyn', sizePrices: ['20/5047.97', '40/10095.94']}, // pralatrexate NOTE DONE
-  {name: 'cryamza', sizePrices: ['100/1109.70', '500/5548.50']}, // ramucirum NOTE DONE
+  {name: 'cyramza', sizePrices: ['100/1109.70', '500/5548.50']}, // ramucirumab NOTE DONE
   {name: 'rituxan', sizePrices: ['100/903.38', '500/4516.90']}, // rituximab NOTE DONE
   {name: 'actemra', sizePrices: ['80/416.80', '200/1057.63', '400/2084']}, //tocilizumab NOTE DONE
   {name: 'hycamtin', sizePrices: ['4/39.20']}, // topotecan NOTE DONE
@@ -45,4 +48,8 @@ module.exports.data = [
   {name: 'vincasar', sizePrices: ['1/5.38', '2/10.00']}, //vincristine NOTE DONE
   {name: 'navelbine', sizePrices: ['10/9.80', '50/37.24']}, // vinorelbine NOTE DONE
   {name: 'zometa', sizePrices: ['4/43.89', '5/111.60']} // zolendric acid NOTE DONE
+
+  /*
+    these are te prices from the email
+  */
 ]
