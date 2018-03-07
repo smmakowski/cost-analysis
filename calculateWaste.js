1f04d9beaa33c8aa0c55d41b468e1ddad803e320
smmakowski <makowski.sm@gmail.com>
2018-03-07 13:53:04 -0800

    add documentation for basic filtering funcs/intro/createVialObject

--- calculateWaste.js
+++ calculateWaste.js
@@ -1,85 +1,69 @@
-// const fs = require('fs');
-// //NOTE: DO NOT DELETE THE BELOW COMMENT AS THIS IS THE MAIN program
-//
-// // get arguments from command line
-// const args = Array.prototype.slice.call(process.argv, 2);
-// // if not enough aruguments print and exit
-// if (args.length < 3) {
-//   console.log('ERROR: Improper number of arguments.');
-//   console.log('Please make sure you have the required arguments: Drug Name, units, \
-//   target dose without units, at least one vial size without units with price (delinated with \'/\')');
-//   console.log('Example: node calculateWaste.js myDrug mg 1000 200/4.35');
-//   process.exit();
-// }
-// // set variables for function
-// const drugName = args[0];
-//
-// const targetDose = args[1] * 1;
-//
-// // create array of objects from vials arguments
-// const vialSizes = args.slice(2).map((item) => {
-//   // if improper format notify and EXIT PROGRAM
-//   if (item.indexOf('/') === -1 || (item.indexOf('/') !== item.lastIndexOf('/'))) {
-//     console.error(`ERROR: argument '${item}' is not formatted properly.`);
-//     console.log('Please make sure that you use the following format <numerical vial size>/<vial price>');
-//     console.log('Example: 1240/923.40');
-//     process.exit();
-//   }
-//   const vial = createVialObject(item, targetDose);
-//
-//   return vial; // return Object
-// });
+/*
+  <<<< INTRODUCTION >>>>
 
-// call calculation function
-// compareWaste(vialSizes, targetDose);
+  The following functions calculate the amount of waste that is generated for a single
+  row in the .CSV File. Calculating the optimal waste is the sole functon. 'createWasteRows.js'
+  contains the logic for iterating through one or more .CSV files, and outputting the results
+  .CSV.
+/*
+  <<<< THE createVialObject() FUNCTION >>>>
 
-// let vial1 = createVialObject("180/2175.22", 1000);
-// let vial2 = createVialObject("260/3483.19", 1000);
-// let vial3 = createVialObject("160/1990.89", 1000);
-// let vial4 = createVialObject("210/1222.91", 1000);
-// let vial5 = createVialObject("140/2002.81", 1000);
-//
-// let vialArr1 = [vial1, vial2, vial3, vial4, vial5];
-// let combosArr = generateCombinations(vialArr1, 1000);
-//
-// createVialCombinationObject(vialArr1, combosArr[0], 1000);
-/* helper function declarations */
+  createVialObject() takes a targetDoes (Number) and a sizePriceString(String) and outputs
+  an object with waste information including: size used (.size), price of the bottle (.price),
+  total units used (.totalUsed), total vials used (.vialsUsed), waste in units (.waste),
+  total cost of administering dose (.totalCost), and money wasted to administed does (.wasteCose).
+
+  The waste levels assume that only one vial size is used to achieve the target dose.
+
+  For example, Drug 'A' multiple sized vials available and have a sizePrices arrays
+  in it's PriceList.js file like so: ['100/20.00', '300/50.00', '1000/75.00'], and the target dose
+  is 1200. In the program createVialObject() will be run for each of these price strings, outputting
+  waste information assuming you used all 100-Vials, then all 300-vials, and then all 1000-vials.
+  You would then get an object for each of these vial sizes. An example of the output object
+  for the 1000 vial would be:
+
+  {size: 1000, price: 75.00, waste: 800, vialsUsed: 2, totalCost: 150.00, wasteCost: 60.00}
+
+  The argument 'targetDose' comes from the function processData() in createWasteRows.js when an
+  indiviudal row is parsed for data extraction
+
+  'sizePriceString' is passed in from (*PriceList.js) when a drug is found during .CSV analysis in
+  in createWasteRows.js.
+*/
 
 function createVialObject(sizePriceString, targetDose) {
-  const values = sizePriceString.split("/");
-  let vial = {};
-  vial.size = values[0] * 1.0;
-  vial.price = values[1] * 1.0;
+  const values = sizePriceString.split("/"); // splits a price string into a tuple [size, price]
+  // NOTE a tuple is an array (basically an ordered list), where each position has a meaning
 
-  // fill bottle
+  let vial = {}; // creates an empty vial object
+  vial.size = values[0] * 1.0; // .size property of vial is set as the size from above 'values' tuple
+  vial.price = values[1] * 1.0; // .price property is set as the price from the about 'values' tuple
+
+  // initialize total medication (in units) and number of vials used as 0
   let totalUsed = 0;
   let vialsUsed = 0;
 
+  // loop that runs while target dose is being filled (while total used is less than the target dose)
   while (totalUsed < targetDose) {
-    totalUsed += vial.size;
-    vialsUsed += 1;
+    totalUsed += vial.size; // add all contents of vial to total used
+    vialsUsed += 1; // add 1 vial to the total count of vials used
   }
-  vial.waste = totalUsed - targetDose;
-  // if no waste vials used are equal else round up for need
+  // NOTE the '+=' used above is an increment operator similar to saying 'totalUsed = totalUsed + vial.size'
 
-  vial.vialsUsed = vialsUsed;
+  vial.waste = totalUsed - targetDose; // target dose subtracted from total used is assigned to .waste property
 
-  vial.totalCost = vial.vialsUsed * vial.price;
-  vial.wasteCost = (vial.waste / vial.size) * vial.price;
-  // split on '/'
-  //console.log('CREATED VIAL: ', vial);
-  return vial;
+  vial.vialsUsed = vialsUsed; // number of vials used is assigned to .vialsUsed property
+
+  vial.totalCost = vial.vialsUsed * vial.price; // .totalCost property is assigned
+  vial.wasteCost = (vial.waste / vial.size) * vial.price; // .wasteCost property is assigned
+  // To clarify calcualtion: (pecentage of a last vial wasted to meet or exceed does) * the price of one vial
+
+  return vial; // return (output) the vial object
 }
 
-
-//TODO: make vialObject class; make vial objectCombo class
-// TODO: make them one and same class, and use arrays for both .size and .vialsUsed properties?
-  // see deadline and assess tech debt
-
-// function to create combinations of vials (NEEDS WORK)
-// trys to meet targetDoes prioritzing least amount of waste and least amount of vials used
-// TODO: get approval for this code, integrate into compare
-// TODO: create logic to generate a vial combo
+/*
+  generateCombos
+*/
 
 function generateCombinations(vialCombinations, targetDose) {
   // declare combo array for return;
@@ -197,6 +181,7 @@ function createVialCombinationObject(sizesArr, combo, targetDose) {
     //console.log(comboObj);
     return comboObj;
 }
+
 // function to find price of vial sized, targetSize in array of vial objects
 function findVialPriceWithSize(arr, targetSize) {
   for (let i = 0; i < arr.length; i++) {
@@ -209,23 +194,66 @@ function findVialPriceWithSize(arr, targetSize) {
   return 0;
 }
 
-function findVialsWithMinWaste(vialCombinations) {
-  let minWasteVial = vialCombinations[0]; // start min at first vial
-  // print information for first item in the sizes
-  //console.log(`The ''${minWasteVial.size} size ${drugName.toUpperCase()}' wastes ${minWasteVial.waste} when attempting a ${targetDose} dose.`);
 
-  // iterate through array to find overall lowest waste amount
+/*
+  <<<< FILETERING WITH findVialsWithMin<something>() FUNCTIONS >>>>
+
+  The following functions: findVialsWithMinWaste(), findVialsWithMinWasteCost(), findVialsWithMinSize(),
+  findCombosWithLeastVialsUsed, and findCombosWithLeastTypesOfVialsUsed(), designed to filter all the
+  combinations of vials used to achieve the target dose passed into compareWaste().
+
+  These functions are used in a sequence in the compareWaste() to find the most optimal combination,
+  considering the following priority:
+
+  1. Minimum waste in units
+  2. Minimum waste cost
+  3. Least amount of total vials used to achieve target dose
+  4. Least number of types of vials used to achieve target dose
+
+  All these functions work in a similar manner. The array of combinations is passed in to the function,
+  and the first combination in the array is set as the one with the minimum desired quality; for example,
+  findVialsWithMinWaste() tries to find the combinations that produce the least waste in whatever units
+  that it comes in, usually mg. It then iterates through the array, changing the the combination that has
+  the minimum desired quality to the one being checked it is lower than the current one. After finding
+  the combination with the desired minimum quality, the array is filtered to only contain combinations
+  that match the optimal vial in terms of the quality being assessed.
+
+  For example, when using the findVialsWithMinCost(), which looks for the combinations that waste
+  the least amount of money, you may have an array of the following combinations
+  (NOTE: the properties have been truncated to illustrate the point):
+
+  [{size: 100, wasteCost: 100.00, ... }, {size: 300, wasteCost: 19.00}, {size: 500, wasteCost: 21.00, ... },
+  {size: 450, wasteCost: 99.00}]
+
+  It will start with the first item ({size: 100, wasteCost: 100.00, ...}) being the one with the lowest, and
+  then go through the rest to compare. When it gets to the second item, the second item becomes the new combination
+  with the least waste cost since the wasteCost is lower. Continuing on it will remain the lowest as nothing seems
+  to beat it. It will then go through one more time removing all the ones that do not have a wasteCost of 19.00,
+  outputting the array [{size: 300, wasteCost 19.00, ...}] at the end of the function.
+
+*/
+
+// Looks for combinations that generate the least amount of waste in units (usually mg)
+// NOTE: this function, as well as findVialsWithMinCost() and findCombosWithLeastVialsUsed are very similar
+// so line by line comments are only used in findVialsWithMinWaste()
+function findVialsWithMinWaste(vialCombinations) {
+  let minWasteVial = vialCombinations[0]; // start with first item in vialCombinations array as lowest waste
+
+  // iterate through array to find overall lowest waste amount, starting with the second item
   for (let i = 1; i < vialCombinations.length; i++) {
+    // if current combination in array produces less waste than current 'minimum waste' combination
     if (vialCombinations[i].waste < minWasteVial.waste) {
-      minWasteVial = vialCombinations[i];
+      minWasteVial = vialCombinations[i]; // set  current item as 'minimum waste combination'
     }
   }
-  // return vials that have minimum waste
+
+  // filter vialCombinations array for vials matching waste levels of minWasteVial and return filtered array
   return vialCombinations.filter((vial) => {
-    return vial.waste === vial.waste;
+    return vial.waste === minWasteVial.waste; // .filter() goes through array and only keeps item where condition is true
   });
 }
 
+// Looks for combinations that generate the least amount of money wasted (in USD$)
 function findVialsWithMinWasteCost(vialCombinations) {
   let minCostVial = vialCombinations[0];
   for (let i = 1; i < vialCombinations.length; i++) {
@@ -239,20 +267,7 @@ function findVialsWithMinWasteCost(vialCombinations) {
   });
 }
 
-function findVialsWithMinSize(vialCombinations) {
-  let smallestVial  = minCostVials[0];
-
-  for (let i = 1; i < minCostVials.length; i++) {
-    if (minCostVials[i].size < smallestVial.size) {
-      smallestVial = minCostVials[i];
-    }
-  }
-
-  return minCostVials.filter((vial) => {
-    return vial.size === smallestVial.size;
-  });
-}
-
+// Looks for combinations that use the least number of total vials
 function findCombosWithLeastVialsUsed(vialCombos) {
   let leastUsed = vialCombos[0];
   for (let i = 1; i < vialCombos.length; i++) {
@@ -260,16 +275,19 @@ function findCombosWithLeastVialsUsed(vialCombos) {
       leastUsed = vialCombos[i];
     }
   }
-//  console.log('LEAST USED =' + leastUsed);
+
   return vialCombos.filter((vial) => {
     return vial.vialsUsed === leastUsed.vialsUsed;
   });
 }
 
+// Looks for combinations that use the least number of types of vials
+// NOTE: this function is slightly different from the other filtering functions so
+// line by line comments will be used.
 function findCombosWithLeastTypesOfVialsUsed(vialCombos) {
   let leastUsedCombo = vialCombos[0];
   let leastUsedAmount;
-//  console.log(leastUsedCombo)
+
   if (leastUsedCombo['sizes']) {
     leastUsedAmount = leastUsedCombo['sizes'].length;
   } else {
@@ -297,10 +315,10 @@ function findCombosWithLeastTypesOfVialsUsed(vialCombos) {
   });
 }
 
-//TODO: add function to fin vials with least vials used();
-
-// function for figurinng out which of the vials that waste the lease volume waste the least money
-// TODO: integrate factored out functions for better readbility
+/*
+  compareWaste() is the main function for determining the optimal waste for one row of .CSV data.
+  It outputs a single object wrapped in an array, which is a
+*/
 
 function compareWaste(vialSizes, targetDose) {
   let combos = generateCombinations(vialSizes, targetDose);
@@ -308,82 +326,16 @@ function compareWaste(vialSizes, targetDose) {
     return createVialCombinationObject(vialSizes, combo, targetDose);
   });
   comboObjs = comboObjs.concat(vialSizes);
-  //console.log('ALL THE COMBOS = ', comboObjs);
 
   let bestVials = findVialsWithMinWasteCost(comboObjs);
-  //console.log('BEST VIALS AFTER MINWASTE COST =' + JSON.stringify(bestVials));
   bestVials = findVialsWithMinWaste(bestVials);
-  //console.log('BEST VIALS AFTER MIN TOTAL WASRE =' + JSON.stringify(bestVials));
   bestVials = findCombosWithLeastVialsUsed(bestVials);
-  //console.log('BEST VIALS AFTER  LEAST NUM OF VIALS USED =' + JSON.stringify(bestVials));
   bestVials = findCombosWithLeastTypesOfVialsUsed(bestVials);
 
-
-  //if still left with multiple results means that reporting twice elimiate the one with sizes arr
-  // if (bestVials.length > 1) {
-  //   bestVials = bestVials.filter((vial) => {
-  //     return !vial.hasOwnProperty('sizes');
-  //   });
-  // }
-
   return [bestVials[0]]; // if there is still one left after this, then just return the 1st one
-
-  //
-  // let minWasteVial = comboObjs[0]; // start min at first vial
-  // // print information for first item in the sizes
-  // //console.log(`The ''${minWasteVial.size} size ${drugName.toUpperCase()}' wastes ${minWasteVial.waste} when attempting a ${targetDose} dose.`);
-  //
-  // // iterate through array to find overall lowest waste amount
-  // for (let i = 1; i < comboObjs.length; i++) {
-  //   if (comboObjs[i].waste < minWasteVial.waste) {
-  //     minWasteVial = comboObjs[i];
-  //   }
-  // }
-  // // filterByVial Sizes
-  // const minWasteVials = comboObjs.filter((vial) => {
-  //   return vial.waste === vial.waste;
-  // });
-  //
-  // if (minWasteVials.length === 1) {
-  //   console.log('BEST VIAL:', minWasteVials[0]);
-  // } else {
-  //   console.log('CHECKING LEAST WASTECOST');
-  //   let minCostVial = minWasteVials[0];
-  //   for (let j = 1; j < minWasteVials.length; j++) {
-  //     if (minWasteVials[j].wasteCost < minCostVial.wasteCost) {
-  //       minCostVial = minWasteVials[j];
-  //     }
-  //   }
-  //
-  //   const minCostVials = minWasteVials.filter((vial) => {
-  //     return vial.wasteCost === minCostVial.wasteCost;
-  //   });
-  //
-  //   if (minCostVials.length === 1) {
-  //     console.log('BEST VIAL:', minCostVials[0]);
-  //   } else {
-  //     console.log('CHECKING SMALLEST VIALS')
-  //     // find smallest comboObjs
-  //     let smallestVial  = minCostVials[0];
-  //
-  //     for (let k = 1; k < minCostVials.length; k++) {
-  //       if (minCostVials[k].size < smallestVial.size) {
-  //         smallestVial = minCostVials[k];
-  //       }
-  //     }
-  //
-  //     const smallestVials = minCostVials.filter((vial) => {
-  //       return vial.size === smallestVial.size;
-  //     });
-  //
-  //     console.log('BEST VIAL(S):');
-  //     smallestVials.forEach((vial) => {
-  //       console.log(vial);
-  //     });
-  //   }
-  // }
 }
 
+// The code below exports the above functions so they can be used by other files in this program.
 module.exports = {
   createVialObject,
   compareWaste,
